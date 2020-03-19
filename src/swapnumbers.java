c89ae8450afed1bdb2d89507f30c82d27aed4b17
Yashaswini99-tech <yashaswiniraju99@gmail.com>
2020-03-20 02:31:35 +0530

    swap2

---
+++ src/swapnumbers.java
@@ -0,0 +1,21 @@
+package com.tgt.igniteplus;
+import java.util.Scanner;
+public class swapnumbers {
+    public static void main(String[] args){
+        int first,second;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the 1st no.");
+        first=sc.nextInt();
+        System.out.println("enter the 2nd no.");
+        second=sc.nextInt();
+        System.out.println("before swap");
+        System.out.println("first number:"+first);
+        System.out.println("second number:"+second);
+        int temp=first;
+        first=second;
+        second=temp;
+        System.out.println("after swap");
+        System.out.println("first number:"+first);
+        System.out.println("second number:"+second);
+    }
+}
